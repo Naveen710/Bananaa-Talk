67a2a678a699dd5b0fe09e9ca30a83b6400d2162
Naveen710 <59551835+Naveen710@users.noreply.github.com>
2022-08-15 21:44:09 +0530

    feature:encoded input

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 /*var userName=prompt("May I know your name please")
-alert("My name is!!!" + userName)*/
+alert("My name is" + userName)*/
 var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
 var Output = document.querySelector("#output")
 
+serverUrl = "https://api.funtranslations.com/translate/minion.json"
 
-console.log(Output)
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text=" +encodeURIComponent(text);
+}
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something went wrong with server")
+}
 
 function clickHandler() {
-    Output.innerText = "een-gggdssa saaajjl "+txtInput.value
+    var inputTxt = txtInput.value;
 
-};
-btnTranslate.addEventListener("click", clickHandler)
+    fetch(getTranslationUrl(inputTxt))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            Output.innerText = translatedText;
+        })
+        .catch(errorHandler)
+    }
+    btnTranslate.addEventListener("click", clickHandler);
